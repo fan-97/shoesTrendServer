d57b873a2e134a0af2c0223830d980585b9a47f9
jie.fan <fann0205@gmail.com>
2021-12-16 19:39:18 +0800

    init

---
+++ src/main/java/com/fanjie/sts/ShoesTrendServerApplication.java
@@ -0,0 +1,13 @@
+package com.fanjie.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesTrendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesTrendServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanjie/sts/ShoesTrendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanjie.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesTrendServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
